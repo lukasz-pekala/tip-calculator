cb2523db8c0691dbddd4936f0baddc937375a4a6
Łukasz Pękala <lukasz-pekala@users.noreply.github.com>
2022-05-28 23:46:39 +0200

    comments

--- src/containers/SummaryContainer.js
+++ src/containers/SummaryContainer.js
@@ -3,12 +3,15 @@ import { Summary } from '../components/Summary';
 
 const mapStateToProps = (state) => {
   const items = state.items;
+
+  // Solution 1:
   //   let subtotal = 0;
 
   //   for (const item of items) {
   //     subtotal += item.price * item.quantity;
   //   }
 
+  // Solution 2:
   const subtotal = items.reduce((acc, item) => {
     return acc + item.price * item.quantity;
   }, 0);
