bf13c5982f2e424ec609d2de5fddf9614bdd6456
Łukasz Pękala <lukasz-pekala@users.noreply.github.com>
2022-05-29 23:25:03 +0200

    checkpoint: KNOWN ISSUE: updating quantity doesn't update the UI

--- src/containers/MenuItemContainer.js
+++ src/containers/MenuItemContainer.js
@@ -5,6 +5,11 @@ import {
   updateQuantity
 } from '../store/items/actions';
 import { MenuItem } from '../components/MenuItem';
+import { selectItemTotal } from '../store/items/selectors';
+
+const mapStateToProps = (state, props) => ({
+  total: selectItemTotal(state, props)
+});
 
 const mapDispatchToProps = (dispatch, ownProps) => {
   return {
@@ -15,4 +20,7 @@ const mapDispatchToProps = (dispatch, ownProps) => {
   };
 };
 
-export const MenuItemContainer = connect(null, mapDispatchToProps)(MenuItem);
+export const MenuItemContainer = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(MenuItem);
--- src/containers/SummaryContainer.js
+++ src/containers/SummaryContainer.js
@@ -1,24 +1,34 @@
 import { connect } from 'react-redux';
 import { Summary } from '../components/Summary';
+import {
+  selectTipAmount,
+  selectSubtotal,
+  selectTotal
+} from '../store/items/selectors';
 
 const mapStateToProps = (state) => {
-  const items = state.items;
-
   // Solution 1:
+  //   const items = state.items;
   //   let subtotal = 0;
 
   //   for (const item of items) {
   //     subtotal += item.price * item.quantity;
   //   }
+  //   const tipAmount = subtotal * (state.tipPercentage / 100);
+  //   const total = subtotal + tipAmount;
 
   // Solution 2:
-  const subtotal = items.reduce((acc, item) => {
-    return acc + item.price * item.quantity;
-  }, 0);
+  //   const items = state.items;
+  //   const subtotal = items.reduce((acc, item) => {
+  //     return acc + item.price * item.quantity;
+  //   }, 0);
+  //   const tipAmount = subtotal * (state.tipPercentage / 100);
+  //   const total = subtotal + tipAmount;
 
-  const tipAmount = subtotal * (state.tipPercentage / 100);
-
-  const total = subtotal + tipAmount;
+  // Solution 3:
+  const subtotal = selectSubtotal(state);
+  const tipAmount = selectTipAmount(state);
+  const total = selectTotal(state);
 
   return { subtotal, tipAmount, total };
 };
